cc9a54174b45b7d99178b8d0c3dd9682c64e7a0e
Will Lehman <william.p.lehman@usace.army.mil>
2021-06-19 10:24:07 -0400

    adding echo context and aws to main

--- compute/compute.go
+++ compute/compute.go
@@ -1,10 +1,15 @@
 package compute
 
 import (
+	"errors"
+
 	"github.com/HydrologicEngineeringCenter/go-fema-consequences/config"
+	"github.com/HydrologicEngineeringCenter/go-fema-consequences/outputwriter"
+	"github.com/HydrologicEngineeringCenter/go-tc-consequences/nhc"
 	consequences_compute "github.com/USACE/go-consequences/compute"
 	"github.com/USACE/go-consequences/consequences"
 	"github.com/USACE/go-consequences/hazardproviders"
+	"github.com/USACE/go-consequences/structureprovider"
 )
 
 type compute struct {
@@ -13,9 +18,81 @@ type compute struct {
 	Ow consequences.ResultsWriter
 }
 
-func Init(c config.Config) compute {
-	return compute{}
+func Init(c config.Config) (compute, error) {
+	var err error
+	var sp consequences.StreamProvider
+	var hp hazardproviders.HazardProvider //not sure what it will be yet, but we can declare it!
+	hazardProviderVerticalUnitsIsFeet := true
+	var ow consequences.ResultsWriter //need a file path to write anything...
+	if c.Sfp != "" {
+		switch c.Ss {
+		case "gpkg":
+			sp = structureprovider.InitGPK(c.Sfp, "nsi")
+		case "shp":
+			sp = structureprovider.InitSHP(c.Sfp)
+		case "nsi":
+			sp = structureprovider.InitNSISP() //default to NSI API structure provider.
+		default:
+			sp = structureprovider.InitNSISP()
+		}
+	} else {
+		sp = structureprovider.InitNSISP()
+	}
+	if c.HpUnits != "" {
+		switch c.HpUnits {
+		case "feet":
+			hazardProviderVerticalUnitsIsFeet = true
+		case "meters":
+			hazardProviderVerticalUnitsIsFeet = false
+		}
+	} else {
+		err = errors.New("cannot compute without hazard provider path")
+	}
+	if c.Hfp != "" {
+		switch c.HpSource {
+		case "nhc":
+			hp = nhc.Init(c.Hfp)
+		case "depths":
+			if hazardProviderVerticalUnitsIsFeet {
+				hp = hazardproviders.Init(c.Hfp)
+			} else {
+				hp = hazardproviders.Init_Meters(c.Hfp)
+			}
+		}
+	} else {
+		err = errors.New("cannot compute without hazard provider path")
+	}
+	ofp := c.Hfp
+	// pull the .tif off the end?
+	ofp = ofp[:len(ofp)-4] //good enough for government work?
+	if ofp != "" {
+		switch c.Ot {
+		case "gpkg":
+			ofp += "_consequences.gpkg"
+			ow = consequences.InitGpkResultsWriter(ofp, "results")
+		case "shp":
+			ofp += "_consequences.shp"
+			ow = consequences.InitShpResultsWriter(ofp, "results")
+		case "geojson":
+			ofp += "_consequences.json"
+			ow = consequences.InitGeoJsonResultsWriterFromFile(ofp)
+		case "summaryDollars":
+			ofp += "_summaryDollars.csv"
+			ow = consequences.InitSummaryResultsWriterFromFile(ofp)
+		case "summaryDepths":
+			ofp += "_summaryDepths.csv"
+			ow = outputwriter.InitSummaryByDepth(ofp)
+		default:
+			ofp += "_consequences.gpkg"
+			ow = consequences.InitGpkResultsWriter(ofp, "results")
+		}
+	} else {
+		err = errors.New("we need an input hazard file path use")
+	}
+	return compute{Hp: hp, Sp: sp, Ow: ow}, err
 }
 func (c compute) Compute() {
+	defer c.Hp.Close()
+	defer c.Ow.Close()
 	consequences_compute.StreamAbstract(c.Hp, c.Sp, c.Ow)
 }
--- main.go
+++ main.go
@@ -1,86 +1,131 @@
 package main
 
 import (
-	"flag"
+	"bytes"
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/HydrologicEngineeringCenter/go-fema-consequences/outputwriter"
-	"github.com/HydrologicEngineeringCenter/go-tc-consequences/nhc"
-	"github.com/USACE/go-consequences/compute"
+	fema_compute "github.com/HydrologicEngineeringCenter/go-fema-consequences/compute"
+	"github.com/HydrologicEngineeringCenter/go-fema-consequences/config"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/lib/pq"
+
 	"github.com/USACE/go-consequences/consequences"
-	"github.com/USACE/go-consequences/hazardproviders"
-	"github.com/USACE/go-consequences/structureprovider"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
+// Config holds all runtime configuration provided via environment variables
+type AWSConfig struct {
+	AWSS3Endpoint       string `envconfig:"AWS_S3_ENDPOINT"`
+	AWSS3Region         string `envconfig:"AWS_S3_REGION"`
+	AWSS3DisableSSL     bool   `envconfig:"AWS_S3_DISABLE_SSL"`
+	AWSS3ForcePathStyle bool   `envconfig:"AWS_S3_FORCE_PATH_STYLE"`
+	AWSS3Bucket         string `envconfig:"AWS_S3_BUCKET"`
+}
+
 func main() {
-	//command line program
-	var sp consequences.StreamProvider
-	var hp hazardproviders.HazardProvider //not sure what it will be yet, but we can declare it!
-	var ow consequences.ResultsWriter     //need a file path to write anything...
-
-	//-ss (structures source) -sfp (structure file path)  -hs (hazard source) -hfp (hazard file path) -ot (output type) //we will define the path internally?
-	sfp := flag.String("sfp", "", "structure file path, (optional)")
-	ss := flag.String("ss", "nsi", "structure source, (optional), acceptable terms: nsi(default), gpkg, shp")
-	hfp := flag.String("hfp", "", "hazard file path, (required)")
-	hs := flag.String("hs", "", "hazard source, (required), acceptable terms: nhc, depths")
-	ot := flag.String("ot", "gpkg", "output type, (optional), acceptable terms: gpkg (default), shp, geojson, summaryDollars, summaryDepths")
-
-	flag.Parse()
-	if *sfp != "" {
-		switch *ss {
-		case "gpkg":
-			sp = structureprovider.InitGPK(*sfp, "nsi")
-		case "shp":
-			sp = structureprovider.InitSHP(*sfp)
-		case "nsi":
-			sp = structureprovider.InitNSISP() //default to NSI API structure provider.
-		default:
-			sp = structureprovider.InitNSISP()
-		}
-	} else {
-		sp = structureprovider.InitNSISP()
+	var cfg AWSConfig
+	if err := envconfig.Process("fema_consequences", &cfg); err != nil {
+		log.Fatal(err.Error())
 	}
-	//fmt.Println(hfp)
-	//fmt.Println(*hfp)
-	if *hfp != "" {
-		switch *hs {
-		case "nhc":
-			hp = nhc.Init(*hfp)
-		case "depths":
-			hp = hazardproviders.Init(*hfp)
-		}
-	} else {
-		panic("cannot compute without hazard provider path, use -h for help.")
-	}
-	ofp := *hfp
-	// pull the .tif off the end?
-	ofp = ofp[:len(ofp)-4] //good enough for government work?
-	fmt.Println(ofp)
-	if ofp != "" {
-		switch *ot {
-		case "gpkg":
-			ofp += "_consequences.gpkg"
-			ow = consequences.InitGpkResultsWriter(ofp, "results")
-		case "shp":
-			ofp += "_consequences.shp"
-			ow = consequences.InitShpResultsWriter(ofp, "results")
-		case "geojson":
-			ofp += "_consequences.json"
-			ow = consequences.InitGeoJsonResultsWriterFromFile(ofp)
-		case "summaryDollars":
-			ofp += "_summaryDollars.csv"
-			ow = consequences.InitSummaryResultsWriterFromFile(ofp)
-		case "summaryDepths":
-			ofp += "_summaryDepths.csv"
-			ow = outputwriter.InitSummaryByDepth(ofp)
-		default:
-			ofp += "_consequences.gpkg"
-			ow = consequences.InitGpkResultsWriter(ofp, "results")
-		}
-	} else {
-		panic("we need an input hazard file path use -h for help.")
-	}
-	defer ow.Close()
-	compute.StreamAbstract(hp, sp, ow)
 
+	// This should probably move elsewhere
+	awsConfig := aws.NewConfig().WithRegion(cfg.AWSS3Region)
+	// Used for "minio" during development
+	awsConfig.WithDisableSSL(cfg.AWSS3DisableSSL)
+	awsConfig.WithS3ForcePathStyle(cfg.AWSS3ForcePathStyle)
+	if cfg.AWSS3Endpoint != "" {
+		awsConfig.WithEndpoint(cfg.AWSS3Endpoint)
+	}
+	newSession, err1 := session.NewSession(awsConfig)
+	if err1 != nil {
+		fmt.Println(err1)
+	}
+	s3c := s3.New(newSession)
+
+	e := echo.New()
+	e.Use(
+		middleware.CORS(),
+		middleware.GzipWithConfig(middleware.GzipConfig{Level: 5}),
+	)
+
+	// Public Routes
+	public := e.Group("")
+
+	// Private Routes
+	/*private := e.Group("")
+	if cfg.SkipJWT == true {
+		private.Use(middleware.MockIsLoggedIn)
+	} else {
+		private.Use(middleware.JWT, middleware.IsLoggedIn)
+	}*/
+
+	// Public Routes
+	// NOTE: ALL GET REQUESTS ARE ALLOWED WITHOUT AUTHENTICATION USING JWTConfig Skipper. See appconfig/jwt.go
+	public.GET("fema-consequences", func(c echo.Context) error {
+		return c.String(http.StatusOK, "fema-consequences-api v0.0.1") //should probably have this pick up from an env variable for version info.
+	})
+	public.GET("fema-consequences/events/", func(c echo.Context) error {
+		resp, err := s3c.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: &cfg.AWSS3Bucket})
+		var list string
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case s3.ErrCodeNoSuchBucket:
+					fmt.Println(s3.ErrCodeNoSuchBucket, aerr.Error())
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+				fmt.Println(err.Error())
+			}
+			return c.String(http.StatusBadRequest, "something bad happened.")
+		}
+		for _, item := range resp.Contents {
+			path := *item.Key
+			if len(path) > 3 {
+				if path[len(path)-3:] == ".eventconfig" {
+					list += path + "\n"
+				}
+			}
+		}
+		return c.String(http.StatusOK, list)
+	})
+	public.POST("fema-consequences/compute", func(c echo.Context) error {
+		var i config.Config
+		if err := c.Bind(&i); err != nil {
+			return c.String(http.StatusBadRequest, "Invalid Input")
+		}
+		c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSONCharsetUTF8)
+		c.Response().WriteHeader(http.StatusOK)
+		compute, err := fema_compute.Init(i)
+		if err != nil {
+			return c.String(http.StatusBadRequest, err.Error())
+		}
+		compute.Compute() //not sure this works with AWS writing... need to rethink how to write geopackage to AWS.
+		return c.String(http.StatusOK, "Compute Complete")
+
+	})
+}
+func writeToS3(vrw consequences.VirtualResultsWriter, s3Path string, cfg AWSConfig, s3c *s3.S3) (string, error) {
+	reader := bytes.NewReader(vrw.Bytes())
+	input := &s3.PutObjectInput{
+		Bucket:        &cfg.AWSS3Bucket,
+		Body:          reader,
+		ContentLength: aws.Int64(int64(reader.Len())),
+		Key:           &s3Path,
+	}
+	s3output, err := s3c.PutObject(input)
+	if err != nil {
+		return "", err
+	}
+	//fmt.Print(s3output)
+	return *s3output.ETag, nil
 }
