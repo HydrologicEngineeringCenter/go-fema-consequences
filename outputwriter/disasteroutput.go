3c13988e939185e522f78ab125f32e3792b213a3
Will Lehman <william.p.lehman@usace.army.mil>
2022-02-14 22:12:40 -0500

    starting the work of making the report

--- outputwriter/disasteroutput.go
+++ outputwriter/disasteroutput.go
@@ -36,6 +36,37 @@ func InitDisasterOutput(filepath string) *disasterOuput {
 	if err != nil {
 		panic(err)
 	}
+	//make fipsmap
+	fm := make(map[string]countyRecord)
+	return &disasterOuput{filepath: filepath, w: w, fipsmap: fm}
+}
+func (srw *summaryByDepth) Write(r consequences.Result) {
+	fips := r.Fetch("cbfips")
+	ssccc := fips[0:5]//first five ditgits of fips are state (2) and county (3)
+	cr, crok := srw.fipsmap[ssccc]
+	if crok{
+		//previously existed, update and reassign.
+		cr.Update(r)
+		srw.fipsmap[ssccc] = cr
+	}else{
+		am := make(map[string]typeRecord)
+		newcr := countyRecord{statefips: fips[0:2], countyfips: fips[2:5], byAssetType: am}
+		newcr.Update(r)
+		srw.fipsmap[ssccc] = newcr
+	}
+}
+func (cr *countyRecord) Update(r consequences.Result){
+	damcat := r.Fetch("damage category")
+	if damcat == "res"{
+		cr.resDamCount +=1
+		cr.resTotDam += r.Fetch("structure damage").(float64)
+		cr.resTotDam += r.Fetch("content damage").(float64)
+	}else{
+		cr.nonresdamcount +=1
+		cr.nonResTotDam += r.Fetch("structure damage").(float64)
+		cr.nonResTotDam += r.Fetch("content damage").(float64)
+	}
+	//add to asset type map.
 	/*
 	Residential
 	Agriculture
@@ -48,11 +79,6 @@ func InitDisasterOutput(filepath string) *disasterOuput {
 	Unclassified
 	Utility and Miscellaneous
 	*/
-	
-	return &disasterOuput{filepath: filepath, w: w}
-}
-func (srw *summaryByDepth) Write(r consequences.Result) {
-
 }
 func (srw *summaryByDepth) Close() {
 /*	fmt.Fprintf(srw.w, "Outcome, Count\n")
